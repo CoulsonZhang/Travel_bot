37a9594429f5973779d476dc28feec4195735438
CoulsonZhang <ying96128@gmail.com>
2020-07-12 16:39:27 -0500

    api COVID_19info function finished

---
+++ play.py
@@ -0,0 +1,3 @@
+name = [{"country":"China","code":"CN","confirmed":88462,"recovered":82808,"critical":3,"deaths":4634,"latitude":35.86166,"longitude":104.195397,"lastChange":"2020-05-18T02:56:52+02:00","lastUpdate":"2020-07-12T23:00:04+02:00"}]
+print(name)
+print(name[0]["confirmed"])
