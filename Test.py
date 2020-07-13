383d13f0fa89630cf932e952e2188aa6130e678c
CoulsonZhang <ying96128@gmail.com>
2020-07-12 21:59:16 -0500

    weather2table optimized

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 import http.client
 import json
-
+import matplotlib.pyplot as plt
 
 def weather2table(cityname):
     conn = http.client.HTTPSConnection("community-open-weather-map.p.rapidapi.com")
@@ -16,38 +16,36 @@ def weather2table(cityname):
     data = res.read().decode("utf-8")
     test = json.loads(data)
 
-    print(data)
-    print(test)
 
+    #initialize the data set
     tem = []
     date = []
+    if test["cod"] == '404':
+        print('please check the name of city')
+    else:
+        for i in range(40):
+            #print("Feeling templature:", end="")
+            num = round(test["list"][i]['main']['feels_like'] - 273.15, 2)
+            #print(num)
+            #print("The date & time of the temperature: ", end="")
+            time = test["list"][i]['dt_txt'].replace('00:00', '00').replace('2020-', '')
+            #print(time)
+            tem.append(num)
+            date.append(time)
+            #print()
 
-    for i in range(40):
-        print("Feeling templature:", end="")
-        num = round(test["list"][i]['main']['feels_like'] - 273.15, 2)
-        print(num)
-        print("The date & time of the temperature: ", end="")
-        time = test["list"][i]['dt_txt'].replace('00:00', '00').replace('2020-', '')
-        print(time)
-        tem.append(num)
-        date.append(time)
-        print()
+        fig = plt.figure(figsize=(15, 6))
+        plt.plot(date,tem,color = "black", linestyle=':')
+        plt.plot(date[:8], tem[:8],label='Day one')
+        plt.plot(date[8:16], tem[8:16],label='Day Two')
+        plt.plot(date[16:24], tem[16:24],label='Day Three')
+        plt.plot(date[24:32], tem[24:32],label='Day Four')
+        plt.plot(date[32:40], tem[32:40],label='Day Five')
+        plt.legend()
+        plt.title('The Forecast of Temperature in 5 Days in ' + cityname.capitalize())
+        plt.xlabel('Date & Time')
+        plt.ylabel('Temperature ℃ ')
+        fig.autofmt_xdate(rotation=45)
+        plt.show()
 
-    print(tem)
-    print(date)
-
-    import matplotlib.pyplot as plt
-    fig = plt.figure(figsize=(15, 6))
-    plt.plot(date[:8], tem[:8], color='black')
-    plt.plot(date[8:16], tem[8:16], color='black')
-    plt.plot(date[16:24], tem[16:24], color='black')
-    plt.plot(date[24:32], tem[24:32], color='black')
-    plt.plot(date[32:40], tem[32:40], color='black')
-
-    plt.title('The Forecast of Temperature in 5 Days in ' + cityname.capitalize())
-    plt.xlabel('Date & Time')
-    plt.ylabel('Temperature ℃ ')
-    fig.autofmt_xdate(rotation=45)
-    plt.show()
-
-weather2table('urbana')
+weather2table('luohe')
